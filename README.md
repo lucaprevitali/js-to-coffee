98552cd61c328ebf8882ed8c26741174373738e2
Luca <lucaprevi@gmail.com>
2019-05-03 22:20:22 +0200

    First release

---
+++ README.md
@@ -0,0 +1,15 @@
+# JsToCoffee
+
+Compile selected JavaScript code to CoffeScript using js2coffee API.
+
+## Features
+
+Compile selected JavaScript code in CoffeeScript, if the selected code have errors an error message with the error details will be displayed in a popup
+
+## Commands and default shortcuts
+
+* **extension.jstocoffee**: `cmd+shift+c` (`ctrl+shift+c` on other systems) or press `cmd+shift+p` and then type **Js To Coffee**
+
+## License
+
+MIT © Luca Previtali
---
+++ src/extension.ts
@@ -0,0 +1,25 @@
+import * as vscode from 'vscode';
+const js2coffee = require('js2coffee');
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let disposable = vscode.commands.registerCommand('extension.jstocoffee', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			let selection = editor.selection;
+			let jsCode = editor.document.getText(selection);
+
+			try {
+				let jsCodeToCoffeeCode = js2coffee.build(jsCode).code;
+				editor.edit(builder => builder.replace(selection, jsCodeToCoffeeCode));
+			} catch (Error) {
+				vscode.window.showErrorMessage(Error.message);
+			}
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+//export function deactivate() {}

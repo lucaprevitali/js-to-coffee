a7ec06034cd7214c05a38862b0ae457dee89431b
Luca <lucaprevi@gmail.com>
2019-05-06 22:21:22 +0200

    minor

--- src/extension.ts
+++ src/extension.ts
@@ -20,6 +20,3 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-//export function deactivate() {}
